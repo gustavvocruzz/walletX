89f7daa75938db2c43f81a6fad4347fdceb78b44
gusttavvo <gusta.gustavocruz@gmail.com>
2026-06-26 14:34:33 -0300

    first commit

---
+++ src/main/java/dev/gustavvocruzz/walletX/WalletXApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavvocruzz.walletX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletXApplication.class, args);
+	}
+
+}
