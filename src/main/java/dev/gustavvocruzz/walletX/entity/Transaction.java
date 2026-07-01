0c903c5522ff8a778b58eac83350cec4bebd43e5
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-01 10:58:10 -0300

    feat: create Transaction Entity

---
+++ src/main/java/dev/gustavvocruzz/walletX/entity/Transaction.java
@@ -0,0 +1,46 @@
+package dev.gustavvocruzz.walletX.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_wallet_id", nullable = false)
+    private Wallet senderWallet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_wallet_id", nullable = false)
+    private Wallet receiverWallet;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+    @Enumerated(value = EnumType.STRING)
+
+    @Column(nullable = false)
+    private TransactionStatus status;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt =LocalDateTime.now();
+    }
+}
