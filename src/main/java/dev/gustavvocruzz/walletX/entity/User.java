469fe89e8354d3985c907dd7f7c7ca467d279ede
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-01 10:40:09 -0300

    create User Entity

---
+++ src/main/java/dev/gustavvocruzz/walletX/entity/User.java
@@ -0,0 +1,72 @@
+package dev.gustavvocruzz.walletX.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+
+    @Column(length = 1)
+    private String gender;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private LocalDate birthday;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = false, unique = true)
+    private String document;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status = UserStatus.ACTIVE;
+
+    @OneToOne(mappedBy = "user")
+    private Wallet wallet;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+
+
+
+
+}
