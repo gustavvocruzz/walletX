466d4ca8973650adf3ab10d4df2089989dbcc260
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-01 10:42:32 -0300

    feat: create Wallet Entity

---
+++ src/main/java/dev/gustavvocruzz/walletX/entity/Wallet.java
@@ -0,0 +1,65 @@
+package dev.gustavvocruzz.walletX.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "wallets")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Column(name = "blocked_balance", nullable = false, precision = 19, scale = 2)
+    private BigDecimal blockedBalance = BigDecimal.ZERO;
+
+    @Column(nullable = false, length = 3)
+    private String currency = "BRL";
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void credit(BigDecimal amount) {
+        this.balance = this.balance.add(amount);
+    }
+
+    public void debit(BigDecimal amount) {
+        if (this.balance.compareTo(amount) < 0) {
+            throw new RuntimeException("Insufficient balance");
+        }
+        this.balance = this.balance.subtract(amount);
+    }
+
+}
