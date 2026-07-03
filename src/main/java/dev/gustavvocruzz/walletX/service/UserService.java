7c0c6506d0d6df6c96831ebc0da862997eef4283
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-03 09:44:53 -0300

    feat: create UserService class

---
+++ src/main/java/dev/gustavvocruzz/walletX/service/UserService.java
@@ -0,0 +1,55 @@
+package dev.gustavvocruzz.walletX.service;
+
+import dev.gustavvocruzz.walletX.entity.User;
+import dev.gustavvocruzz.walletX.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+
+    private final UserRepository repository;
+    private final WalletService walletService;
+
+
+    //TODO: CRIAR USER
+    //TODO: ATUALIZAR USER
+    //TODO: VALIDATIONS
+
+    @Transactional
+    public User createUser(User user){
+
+        validateUser(user);
+
+        //TODO: Criptografar senha
+
+
+        User savedUser = repository.save(user);
+
+        walletService.createWallet(savedUser);
+
+        return savedUser;
+    }
+
+
+    //Valida o usuario
+    //TODO: Criar tratamento de excecoes
+
+    private void validateUser(User user){
+        if(repository.existsByDocument(user.getDocument())){
+            throw new RuntimeException("Já existe um usuario cadastrado com esse documento.");
+        }
+
+        if(repository.existsByEmail(user.getEmail())){
+            throw new RuntimeException("Já existe um usuario cadastrado com esse e-mail.");
+        }
+
+        if(repository.existsByPhone(user.getPhone())){
+            throw new RuntimeException("Já existe um usuario cadastrado com esse telefone.");
+        }
+
+    }
+}
