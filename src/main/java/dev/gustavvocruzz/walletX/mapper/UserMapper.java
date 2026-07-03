0dc3eec28f6413f280b4ad1e5b2a0fb7f070dd39
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-03 09:44:27 -0300

    feat: create UserMapper class and methods toEntity and toResponse

---
+++ src/main/java/dev/gustavvocruzz/walletX/mapper/UserMapper.java
@@ -0,0 +1,43 @@
+package dev.gustavvocruzz.walletX.mapper;
+
+import dev.gustavvocruzz.walletX.dtos.request.UserRequest;
+import dev.gustavvocruzz.walletX.dtos.response.UserResponse;
+import dev.gustavvocruzz.walletX.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserRequest request){
+
+        var entity = new User();
+        entity.setFirstName(request.firstName());
+        entity.setLastName(request.lastName());
+        entity.setGender(request.gender());
+        entity.setEmail(request.email());
+        entity.setBirthday(request.birthday());
+        entity.setPassword(request.password());
+        entity.setPhone(request.phone());
+        entity.setDocument(request.document());
+
+
+        return entity;
+    }
+
+    public UserResponse toResponse(User user){
+        return new UserResponse(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getDocument(),
+                user.getStatus(),
+                user.getWallet().getId(),
+                user.getCreatedAt()
+        );
+
+    }
+}
