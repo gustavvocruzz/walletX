87cb6c81b6f26cdfea6831ffc20f37d1e572adb2
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-04 14:17:40 -0300

    feat: create ErrorResponse

---
+++ src/main/java/dev/gustavvocruzz/walletX/exceptions/ErrorResponse.java
@@ -0,0 +1,12 @@
+package dev.gustavvocruzz.walletX.exceptions;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {
+}
