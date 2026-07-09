f9c6bfd8c80e781e62efd883d6ce206e710874f2
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-09 12:34:08 -0300

    feat: create TransactionRepository

---
+++ src/main/java/dev/gustavvocruzz/walletX/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package dev.gustavvocruzz.walletX.repositories;
+
+import dev.gustavvocruzz.walletX.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
