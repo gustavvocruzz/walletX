6215e04b4947db6d77dd88d91ea935a6d962b1f5
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-09 12:30:08 -0300

    feat: create WalletBlockedException

---
+++ src/main/java/dev/gustavvocruzz/walletX/exceptions/WalletBlockedException.java
@@ -0,0 +1,7 @@
+package dev.gustavvocruzz.walletX.exceptions;
+
+public class WalletBlockedException extends RuntimeException {
+    public WalletBlockedException(String message) {
+        super(message);
+    }
+}
