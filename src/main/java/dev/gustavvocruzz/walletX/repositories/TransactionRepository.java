c6e9b3c6b817875908bf72c3c65c96039fe465e9
gusttavvo <gusta.gustavocruz@gmail.com>
2026-07-10 11:02:17 -0300

    feat: add search Transaction by Wallet id

--- src/main/java/dev/gustavvocruzz/walletX/repositories/TransactionRepository.java
+++ src/main/java/dev/gustavvocruzz/walletX/repositories/TransactionRepository.java
@@ -2,8 +2,18 @@ package dev.gustavvocruzz.walletX.repositories;
 
 import dev.gustavvocruzz.walletX.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    @Query("""
+    SELECT t
+    FROM Transaction t
+    WHERE t.senderWallet.id = :walletId
+       OR t.receiverWallet.id = :walletId
+    ORDER BY t.createdAt DESC
+""")
+    List<Transaction> findAllByWalletId(UUID walletId);
 }
